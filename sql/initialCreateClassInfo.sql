b82a715a18ed84ecc912b6d739644cce7bc6befd
Ethan Hampton <etho@ethohampton.com>
2022-07-28 18:46:17 -0500

    Add Winter and Spring 2022 data

--- sql/initialCreateClassInfo.sql
+++ sql/initialCreateClassInfo.sql
@@ -1,4 +1,4 @@
---Create/update class info table, keeping class name the same if it has already been filled
+-- Create/update class info table, keeping class name the same if it has already been filled
 REPLACE INTO ClassInfo(ClassIdentifier, Credits, ClassName)
 SELECT
     InfoFromClasses.ClassIdentifier,
