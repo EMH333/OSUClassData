be20633cc3b17e71e68d000b735fb46dec14b484
Ethan Hampton <etho@ethohampton.com>
2023-05-19 18:38:35 -0700

    Add new task queue implementation for future use
    This will replace the jank that is the class names API queries as well as allow some additional queues for other data processing

---
+++ internal/util/taskQueue.go
@@ -0,0 +1,87 @@
+package util
+
+import (
+	"context"
+	"database/sql"
+	"errors"
+	"github.com/enriquebris/goconcurrentqueue"
+	"time"
+)
+
+type TaskQueue struct {
+	internalQueue *goconcurrentqueue.FixedFIFO
+	taskRunning   bool
+	//stateMutex    sync.Mutex
+	task func(db *sql.DB, item interface{}, queue *TaskQueue) TaskQueueReturn
+	db   *sql.DB
+
+	WaitDuration time.Duration
+
+	//stats
+	processedItems uint64
+}
+
+type TaskQueueReturn struct {
+	// if true, queue won't wait before processing next item
+	noWait bool
+}
+
+type TaskQueueImp interface {
+	Enqueue(item interface{}) error
+	runQueue()
+}
+
+func NewTaskQueue(db *sql.DB, task func(db *sql.DB, item interface{}, queue *TaskQueue) TaskQueueReturn, waitDuration time.Duration, maxSize int) *TaskQueue {
+	return &TaskQueue{
+		internalQueue: goconcurrentqueue.NewFixedFIFO(maxSize),
+		taskRunning:   false,
+		//stateMutex: sync.Mutex{},
+		task:           task,
+		db:             db,
+		WaitDuration:   waitDuration,
+		processedItems: 0,
+	}
+}
+
+func (q *TaskQueue) Enqueue(item interface{}) error {
+	if q.internalQueue.GetLen() == q.internalQueue.GetCap() {
+		return errors.New("queue full")
+	}
+
+	err := q.internalQueue.Enqueue(item)
+	if err != nil {
+		return err
+	}
+
+	if !q.taskRunning {
+		go q.runQueue()
+		q.taskRunning = true
+	}
+
+	return nil
+}
+
+func (q *TaskQueue) runQueue() {
+	// if this func exits, make sure we know to start a new one
+	defer func() {
+		q.taskRunning = false
+	}()
+	for {
+		ctx, cancel := context.WithTimeout(context.Background(), q.WaitDuration*2)
+		element, err := q.internalQueue.DequeueOrWaitForNextElementContext(ctx)
+		if err != nil {
+			q.taskRunning = false
+			cancel()
+			return // return if we don't have any more elements to process
+		}
+		cancel()
+
+		// run the actual task
+		ret := q.task(q.db, element, q)
+		q.processedItems++
+
+		if !ret.noWait {
+			time.Sleep(q.WaitDuration)
+		}
+	}
+}
---
+++ internal/util/taskQueue_test.go
@@ -0,0 +1,56 @@
+package util
+
+import (
+	"database/sql"
+	"testing"
+	"time"
+)
+
+func TestTaskQueue(t *testing.T) {
+	tq := NewTaskQueue(&sql.DB{}, func(db *sql.DB, item interface{}, queue *TaskQueue) TaskQueueReturn {
+		//if we send a bool as an item, it won't wait to process the next item
+		if v, ok := item.(bool); ok && v {
+			return TaskQueueReturn{
+				noWait: true,
+			}
+		}
+		return TaskQueueReturn{
+			noWait: false,
+		}
+	}, time.Millisecond*15, 3)
+
+	//The first one will get run immediately so 4 need to be added to saturate queue
+	for i := 0; i < 4; i++ {
+		err := tq.Enqueue(false)
+		if err != nil {
+			t.Errorf("Should not have failed to enqueue")
+		}
+		time.Sleep(time.Millisecond)
+	}
+
+	err := tq.Enqueue(false)
+	if err == nil {
+		t.Errorf("Should have failed to enqueue once queue is full")
+	}
+
+	time.Sleep(time.Millisecond * 80)
+
+	//the item that failed to enqueue doesn't count
+	if tq.processedItems != 4 {
+		t.Errorf("Should have processed 4 items by now")
+	}
+
+	//check no wait
+	for i := 0; i < 10; i++ {
+		err := tq.Enqueue(true)
+		if err != nil {
+			t.Errorf("Should not have failed to enqueue")
+		}
+		time.Sleep(time.Millisecond)
+	}
+	time.Sleep(time.Millisecond * 20) // this should be plenty given they aren't waiting in between
+	if tq.processedItems != 14 {
+		t.Errorf("Should have processed 14 items by now")
+	}
+
+}
